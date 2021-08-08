dd493f8b08b7ce92bf77e34bd948e9b42693badb
r.ivanov <r.ivanov@corp.vk.com>
2021-08-08 23:29:50 +0300

    test on other browsers

--- java-example/src/test/java/ru/stqa/training/selenium/LoginTests.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTests.java
@@ -3,30 +3,55 @@ package ru.stqa.training.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTests {
-    private WebDriver driver;
+    private WebDriver chromeDriver;
+    private WebDriver firefoxDriver;
+    private WebDriver safariDriver;
+
 
 
     @BeforeTest
     public void start(){
-        driver = new ChromeDriver();
+        chromeDriver = new ChromeDriver();
+        firefoxDriver = new FirefoxDriver();
+        safariDriver = new SafariDriver();
     }
 
-    @Test
-    public void testLogin(){
+//    @Test
+    public void testLogin(WebDriver driver){
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
     }
 
+
+    @Test
+    public void testChrome(){
+        testLogin(chromeDriver);
+    }
+
+    @Test
+    public void testFirefox(){
+        testLogin(firefoxDriver);
+    }
+
+    @Test
+    public void testSafari(){
+        testLogin(safariDriver);
+    }
+
+
     @AfterTest
     public void stop(){
-        driver.quit();
-        driver = null;
+        chromeDriver.quit();
+        firefoxDriver.quit();
+        safariDriver.quit();
     }
 }
